f61fc925614b2d1f7880fd4d0bdda890dd2db025
adrichey <1534554+adrichey@users.noreply.github.com>
2026-03-21 17:12:07 -0500

    Dynamically create our help output

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 	"strings"
 
 	"github.com/adrichey/cbconv/cbpdf"
+	"github.com/adrichey/cbconv/project"
 )
+
+const OUTPUT_DIR = "cbconv_output"
+const inputHelpText = "Path to a comic book archive file or directory containing comic book archive files. Accepts: .cb7, .cbr"
+
+var outputHelpText = fmt.Sprintf("Specify an output file or directory for the converted PDF(s). Defaults to a sibling directory beside input path named %s", OUTPUT_DIR)
 
 var help bool
 var input string
@@ -16,13 +22,18 @@ var output string
 
 func init() {
 	flag.BoolVar(&help, "help", false, "Help")
-	flag.StringVar(&input, "i", "", "Path to a comic book archive file (.cb7, .cba, .cbr, .cbt, .cbz)")
-	flag.StringVar(&output, "o", "", "Specify an output file for the converted PDF")
+	flag.StringVar(&input, "i", "", inputHelpText)
+	flag.StringVar(&output, "o", "", outputHelpText)
 
 	flag.Parse()
 }
 
 func main() {
+	if input == "" && output == "" {
+		displayHelp()
+		return
+	}
+
 	if help {
 		displayHelp()
 		return
@@ -63,10 +74,10 @@ func main() {
 		return
 	}
 
-	// Use -o as output directory if provided, otherwise default to sibling cbconv_output
+	// Use -o as output directory if provided, otherwise default to sibling directory with name OUTPUT_DIR
 	outDir := output
 	if outDir == "" {
-		outDir = filepath.Join(filepath.Dir(filepath.Clean(input)), "cbconv_output")
+		outDir = filepath.Join(filepath.Dir(filepath.Clean(input)), OUTPUT_DIR)
 	}
 	if err := os.MkdirAll(outDir, 0755); err != nil {
 		fmt.Println("Error creating output directory:", err)
@@ -92,15 +103,40 @@ func main() {
 
 func displayHelp() {
 	fmt.Println("How to use this script:")
-	fmt.Println("-i: Path to a comic book archive file (.cb7, .cba, .cbr, .cbt, .cbz) or a directory containing comic archive files")
-	fmt.Println("-o: Specify an output file for the converted PDF")
+	fmt.Printf("-i: %s\n", inputHelpText)
+	fmt.Printf("-o: %s\n", outputHelpText)
+
+	ex, err := os.Executable()
+	if err != nil {
+		return
+	}
+
+	ex = filepath.Base(ex)
+
 	fmt.Println()
-	fmt.Println("Example:")
-	fmt.Println("./cbconv -i ./path/to/file/example.cbz")
-	fmt.Println("This will save the converted file to ./path/to/file/example.pdf")
+
+	fmt.Println("Example - Single File:")
+	formattedString := fmt.Sprintf("%s -i .|path|to|file|example.cbz", ex)
+	fmt.Println(strings.ReplaceAll(formattedString, "|", project.PATH_SEPARATOR))
+	fmt.Println(strings.ReplaceAll("This will save the converted file to .|path|to|file|example.pdf", "|", project.PATH_SEPARATOR))
 	fmt.Println()
-	fmt.Println("Example with optional args:")
-	fmt.Println("./cbconv -i ./path/to/file/example.cbz -o ./converted/comic.pdf")
-	fmt.Println("This will save the converted file to ./converted/comic.pdf")
+
+	fmt.Println("Example - Directory:")
+	formattedString = fmt.Sprintf("%s -i .|one|two|comics", ex)
+	fmt.Println(strings.ReplaceAll(formattedString, "|", project.PATH_SEPARATOR))
+	formattedString = fmt.Sprintf("This will save the converted files to .|one|two|%s", OUTPUT_DIR)
+	fmt.Println(strings.ReplaceAll(formattedString, "|", project.PATH_SEPARATOR))
+	fmt.Println()
+
+	fmt.Println("Example - Single File with Specified Output File:")
+	formattedString = fmt.Sprintf("%s -i .|path|to|file|example.cbz -o .|converted|comic.pdf", ex)
+	fmt.Println(strings.ReplaceAll(formattedString, "|", project.PATH_SEPARATOR))
+	fmt.Println(strings.ReplaceAll("This will save the converted file to .|converted|comic.pdf", "|", project.PATH_SEPARATOR))
+	fmt.Println()
+
+	fmt.Println("Example - Directory with Specified Output Directory:")
+	formattedString = fmt.Sprintf("%s -i .|one|two|comics -o .|one|converted_comics", ex)
+	fmt.Println(strings.ReplaceAll(formattedString, "|", project.PATH_SEPARATOR))
+	fmt.Println(strings.ReplaceAll("This will save the converted files to .|one|converted_comics", "|", project.PATH_SEPARATOR))
 	fmt.Println()
 }
